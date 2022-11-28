3e721915896ba1a26d1c8473e7dccba0a2cb4689
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2022-11-29 00:12:20 +0900

    [fix] コンテナの外からnuxt.jsにアクセスできるように変更

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -1,4 +1,7 @@
 export default {
+  server: {
+    host: '0.0.0.0' // デフォルト: localhost
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Read-Recode-On-Nuxt',
